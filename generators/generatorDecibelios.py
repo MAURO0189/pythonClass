1124c7e97d46d3bfeb6ae5c5920a455a57de2ec6
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-06-25 16:57:38 -0500

    add new package

--- generators/generatorDecibelios.py
+++ generators/generatorDecibelios.py
@@ -6,7 +6,7 @@ def generateDecibelios():
         data={}
         id=random.randint(1,50000)
         levelNoise=random.randint(10,90)
-        comuna=random.choice(["comuna 1","comuna 2","comuna 3","comuna 4","comuna 5","comuna 6","comuna 7","comuna 8","comuna 9","comuna 10","comuna 11","comuna 12","comuna 13","comuna 14","comuna 15","comuna 16"])
+        comuna=random.choice(["comuna 1","comuna 2","comuna 3","comuna 4","comuna 5","comuna 6","comuna 7","comuna 8","comuna 9","comuna 10","comuna 11","comuna 12","comuna 13","comuna 14","comuna 15","comuna 16","sin","-","nan"])
         data=[id,levelNoise,comuna]
         noiseSurvey.append(data)
     return(noiseSurvey)
